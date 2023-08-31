e40d1b9d387477fa847e06c6a726e4b929420b01
dilan-schintha97 <dil97an@gmail.com>
2023-08-31 23:02:47 +0530

    done create product and get all products

--- product-service/src/main/java/com/spark/productservice/service/ProductService.java
+++ product-service/src/main/java/com/spark/productservice/service/ProductService.java
@@ -1,16 +1,22 @@
 package com.spark.productservice.service;
 
 import com.spark.productservice.dto.ProductRequest;
+import com.spark.productservice.dto.ProductResponse;
 import com.spark.productservice.model.Product;
 import com.spark.productservice.model.repo.ProductRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProductService {
+
     private final ProductRepo productRepo;
 
     public void createProduct(ProductRequest productRequest){
@@ -24,6 +30,22 @@ public class ProductService {
         log.info("Product {} is saved",product.getId());
     }
 
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> productList = productRepo.findAll();
+
+        Stream<ProductResponse> productResponseStream = productList.stream().map(product -> mapToProductResponse(product));
+        return productResponseStream.collect(Collectors.toList());
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
 
 
 }
