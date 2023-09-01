38ad50fed388826992ba25c9c00bf4e110af33ce
dilan-schintha97 <dil97an@gmail.com>
2023-09-01 23:48:10 +0530

    2

--- product-service/src/test/java/com/spark/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/spark/productservice/ProductServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.spark.productservice;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spark.productservice.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,6 +15,8 @@ import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.math.BigDecimal;
+
 @SpringBootTest
 @Testcontainers
 @AutoConfigureMockMvc
@@ -24,19 +28,32 @@ class ProductServiceApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
 		dynamicPropertyRegistry.add("spring.data.mongodb.url", mongoDBContainer::getReplicaSetUrl);
 	}
 
 	@Test
-	void shouldCreateProduct() {
+	void shouldCreateProduct() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
 		mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
 				.contentType(MediaType.APPLICATION_JSON)
-				.content()
+				.content(productRequestString))
+				.andExpect(staus().isCreated());
 
-
-		);
 	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iphone 13")
+				.description("iphone 13")
+				.price(BigDecimal.valueOf(1110000))
+				.build();
+	}
+
 
 }
